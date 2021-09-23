ed1573f0385b92810f058af21d625f72f8096098
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2021-09-24 01:15:37 +0530

    Use this file with lc_35_access_modifiers.java

---
+++ checking.java
@@ -0,0 +1,10 @@
+package com.saqib;
+public class checking {
+    int a1=4;
+    public int b1=3;
+    protected int c1=2;
+    private int d1=1;
+    public static void main(String[] args) {
+
+    }
+}
