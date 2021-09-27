c8e1b49792c6e2c35a6579953e6b2548f9a75db9
Mohammad Saqib <87147700+Mohammad-Saqib1@users.noreply.github.com>
2021-09-28 01:08:10 +0530

    Create lc_48_collection_framework.java

---
+++ lc_48_collection_framework.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class lc_48_collection_framework {
+    public static void main(String[] args) {
+        System.out.println("Collection Framework");
+        //ArrayList
+        //Set
+        //TreeSet
+
+    }
+}
